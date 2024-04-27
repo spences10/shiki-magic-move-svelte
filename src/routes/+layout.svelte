16c10c5fa1d1d53902cc9296e5f1976ccdbf19c1
spences10 <spences10apps@gmail.com>
2024-04-27 14:37:18 +0100

    feat: :wrench: add tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import '../app.pcss';
 	import '../shiki-styles.css';
 
 	let { children } = $props();
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,27 +3,27 @@
 	import { getHighlighter } from 'shiki';
 
 	let code_from = `
-  let search = ''
-  $: filteredCodes = codes.filter(
-    item =>
-      item.code.toLowerCase().includes(search.toLowerCase()) ||
-      item.message.toLowerCase().includes(search.toLowerCase()) ||
-      item.detail.toLowerCase().includes(search.toLowerCase()) ||
-      item.class.toLowerCase().includes(search.toLowerCase())
-  )
+    let search = ''
+    $: filteredCodes = codes.filter(
+      item =>
+        item.code.toLowerCase().includes(search.toLowerCase()) ||
+        item.message.toLowerCase().includes(search.toLowerCase()) ||
+        item.detail.toLowerCase().includes(search.toLowerCase()) ||
+        item.class.toLowerCase().includes(search.toLowerCase())
+    )
   `;
 	let code_to = `
-  let search_query = $state('');
-  let filtered_codes = $derived.by(() => {
-    if (search_query.length === 0) return codes;
-    return codes.filter(
-      (item) =>
-        item.code.toLowerCase().includes(search_query.toLowerCase()) ||
-        item.message.toLowerCase().includes(search_query.toLowerCase()) ||
-        item.detail.toLowerCase().includes(search_query.toLowerCase()) ||
-        item.class.toLowerCase().includes(search_query.toLowerCase())
-    );
-  });
+    let search_query = $state('');
+    let filtered_codes = $derived.by(() => {
+      if (search_query.length === 0) return codes;
+      return codes.filter(
+        (item) =>
+          item.code.toLowerCase().includes(search_query.toLowerCase()) ||
+          item.message.toLowerCase().includes(search_query.toLowerCase()) ||
+          item.detail.toLowerCase().includes(search_query.toLowerCase()) ||
+          item.class.toLowerCase().includes(search_query.toLowerCase())
+      );
+    });
   `;
 
 	let toggle = $state(false);
@@ -34,19 +34,27 @@
 		langs: ['javascript', 'typescript', 'svelte']
 	});
 
+	const clean_tabs = (code: string) => {
+		const tabs = code
+			.split('\n')
+			.filter(Boolean)[0]
+			.split('')
+			.filter((char) => char === '\t')
+			.join('');
+		return code
+			.split(/(\n)/)
+			.map((line) => line.replace(tabs, ''))
+			.join('');
+	};
+
 	const toggle_animation = () => {
 		toggle = !toggle;
-		toggle ? (code = code_to) : (code = code_from);
+		toggle ? (code = clean_tabs(code_to)) : (code = clean_tabs(code_from));
 	};
 </script>
 
 <svelte:window on:click={toggle_animation} />
 
 {#await highlighter then highlighter}
-	<ShikiMagicMove
-		lang="ts"
-		theme="night-owl"
-		{highlighter}
-		{code}
-	/>
+	<ShikiMagicMove lang="ts" theme="night-owl" {highlighter} {code} />
 {/await}
